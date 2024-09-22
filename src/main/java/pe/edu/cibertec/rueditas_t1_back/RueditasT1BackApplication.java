c958029cf912f0b5a54f703a2d56a1464a20283a
Rosa Izquierdo <i202212506@cibertec.edu.pe>
2024-09-22 18:47:36 -0500

    Creación de proyecto

---
+++ src/main/java/pe/edu/cibertec/rueditas_t1_back/RueditasT1BackApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_t1_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RueditasT1BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RueditasT1BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pe/edu/cibertec/rueditas_t1_back/RueditasT1BackApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.rueditas_t1_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RueditasT1BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
